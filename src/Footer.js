28a1a32bcd9e106ff8e4622a9d3457916d69d821
jam4jm <107881114+jam4jm@users.noreply.github.com>
2022-11-28 23:47:57 +0200

    github project link updated

--- src/Footer.js
+++ src/Footer.js
@@ -4,8 +4,11 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <footer>
-      <a href="https://github.com/jam4jm/WeatherCheck"> Open-source code</a> by
-      Yuliia
+      <a href="https://github.com/jam4jm/weather-app-react">
+        {" "}
+        Open-source code
+      </a>{" "}
+      by Yuliia
     </footer>
   );
 }
